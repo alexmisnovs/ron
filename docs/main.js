2ad47c69b937466ca81fa71d36f63aec37baaaae
alexmisnovs <aleksandrmisnov@gmail.com>
2019-10-11 16:07:52 +0100

    Rons first commit

---
+++ docs/main.js
@@ -0,0 +1,12 @@
+var jokeDiv = document.getElementById('joke');
+
+const generateJoke = () => {
+    fetch("http://api.icndb.com/jokes/random?firstName=Ron&lastName=Bearningsworth&limitTo=[explicit]")
+        .then((resp) => resp.json()) // Transform the data into json
+        .then(function (data) {
+            // Create and append the li's to the ul
+            jokeDiv.innerHTML = `<p>${data.value.joke}</p>`
+            console.log(data.value.joke);
+        })
+}
+
