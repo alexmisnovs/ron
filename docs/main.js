65a1eb4c5fb8e783d996ffab3c6dacd096a44b6b
alexmisnovs <aleksandrmisnov@gmail.com>
2019-10-11 17:28:24 +0100

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,12 +1,21 @@
 var jokeDiv = document.getElementById('joke');
 
-const generateJoke = () => {
-    fetch("https://api.icndb.com/jokes/random?firstName=Ron&lastName=Bearningsworth&exclude=[nerdy]")
+const generateJoke = (type) => {
+
+    let url = "https://api.icndb.com/jokes/random?firstName=Ron&lastName=Bearningsworth";
+    if (type == 'explicit') {
+        url += '&limitTo=[explicit]'
+    }
+    else if (url == 'nerdy') {
+        url += '&limitTo=[nerdy]'
+    }
+    fetch(url)
         .then((resp) => resp.json()) // Transform the data into json
         .then(function (data) {
             // Create and append the li's to the ul
             jokeDiv.innerHTML = `<p>${data.value.joke}</p>`
-            //console.log(data.value.joke);
+            console.log(data.value.joke);
         })
 }
+
 
